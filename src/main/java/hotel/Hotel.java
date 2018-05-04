af515d13d2ed790f5dcac0437fa69cb728deb78e
Brian Cooke <brian.cooke@live.co.uk>
2018-05-04 22:17:31 +0100

    Added methods and tests to check for vacant dining and conference rooms.

--- src/main/java/hotel/Hotel.java
+++ src/main/java/hotel/Hotel.java
@@ -9,71 +9,92 @@ import java.util.ArrayList;
 
 public class Hotel {
 
+    ArrayList<BedRoom> bedRooms;
     ArrayList<ConferenceRoom> conferenceRooms;
     ArrayList<DiningRoom> diningRooms;
-    ArrayList<BedRoom> bedRooms;
 
     public Hotel() {
+        this.bedRooms = new ArrayList<>();
         this.conferenceRooms = new ArrayList<>();
         this.diningRooms = new ArrayList<>();
-        this.bedRooms = new ArrayList<>();
-    }
-
-    public ArrayList<ConferenceRoom> getConferenceRooms() {
-        return conferenceRooms;
-    }
-
-    public void addConferenceRooms(ConferenceRoom conferenceRoom) {
-        this.conferenceRooms.add(conferenceRoom);
-    }
-
-    public int countConferenceRooms() {
-        return this.getConferenceRooms().size();
-    }
-
-    public ArrayList<DiningRoom> getDiningRooms() {
-        return diningRooms;
-    }
-
-    public void addDiningRooms(DiningRoom diningRoom) {
-        this.diningRooms.add(diningRoom);
-    }
-
-    public int countDiningRooms() {
-        return getDiningRooms().size();
     }
 
     public ArrayList<BedRoom> getBedRooms() {
         return bedRooms;
     }
 
+    public ArrayList<ConferenceRoom> getConferenceRooms() {
+        return conferenceRooms;
+    }
+
+    public ArrayList<DiningRoom> getDiningRooms() {
+        return diningRooms;
+    }
+
     public void addBedRooms(BedRoom bedRoom) {
         this.bedRooms.add(bedRoom);
+    }
+
+    public void addConferenceRooms(ConferenceRoom conferenceRoom) {
+        this.conferenceRooms.add(conferenceRoom);
+    }
+
+    public void addDiningRooms(DiningRoom diningRoom) {
+        this.diningRooms.add(diningRoom);
     }
 
     public int countBedRooms() {
         return this.getBedRooms().size();
     }
 
-    public void checkGuestIn(BedRoom bedRoom, Guest guest, int numberOfNights) {
+    public int countConferenceRooms() {
+        return this.getConferenceRooms().size();
+    }
+
+    public int countDiningRooms() {
+        return getDiningRooms().size();
+    }
+
+    public void checkGuestInToBedRoom(BedRoom bedRoom, Guest guest, int numberOfNights) {
         bedRoom.addGuestToRoom(guest, numberOfNights);
+    }
+
+    public void admitGuestToRoom(Room room, Guest guest) {
+        room.addGuestToRoom(guest);
     }
 
     public void checkGuestOut(Room room) {
         room.removeGuestFromRoom();
     }
 
-    public void admitGuestToRoom(Room conferenceRoom, Guest guest) {
-        conferenceRoom.addGuestToRoom(guest);
-    }
-
     public ArrayList<Room> getVacantBedRooms() {
-        ArrayList emptyRooms = new ArrayList<Room>();
+        ArrayList emptyBedRooms = new ArrayList<Room>();
         for (BedRoom bedroom : bedRooms) {
             if (bedroom.countGuestsInRoom() == 0) {
-                emptyRooms.add(bedroom);
+                emptyBedRooms.add(bedroom);
             }
         }
-        return emptyRooms;
+        return emptyBedRooms;
     }
+
+    public ArrayList<Room> getVacantConferenceRooms() {
+        ArrayList emptyConferenceRooms = new ArrayList<Room>();
+        for (ConferenceRoom conferenceRoom : conferenceRooms) {
+            if (conferenceRoom.countGuestsInRoom() == 0) {
+                emptyConferenceRooms.add(conferenceRoom);
+            }
+        }
+        return emptyConferenceRooms;
+    }
+
+    public ArrayList<Room> getVacantDiningRooms() {
+        ArrayList emptyDiningRooms = new ArrayList<Room>();
+        for (DiningRoom diningRoom : diningRooms) {
+            if (diningRoom.countGuestsInRoom() == 0) {
+                emptyDiningRooms.add(diningRoom);
+            }
+        }
+        return emptyDiningRooms;
+    }
+
 }
--- src/test/java/HotelTest.java
+++ src/test/java/HotelTest.java
@@ -15,8 +15,10 @@ public class HotelTest {
     Hotel hotel;
     BedRoom bedRoom1;
     BedRoom bedRoom2;
-    ConferenceRoom conferenceRoom;
-    DiningRoom diningRoom;
+    ConferenceRoom conferenceRoom1;
+    ConferenceRoom conferenceRoom2;
+    DiningRoom diningRoom1;
+    DiningRoom diningRoom2;
     Guest guest1;
     Guest guest2;
 
@@ -25,10 +27,14 @@ public class HotelTest {
         hotel = new Hotel();
         bedRoom1 = new BedRoom(RoomType.SINGLE, 1, 40.00);
         bedRoom2 = new BedRoom(RoomType.DOUBLE, 2, 50.00);
-        conferenceRoom = new ConferenceRoom(1, "North Conference Room", 250.00 );
-        diningRoom = new DiningRoom(1, "Fine Dining");
-        hotel.addDiningRooms(diningRoom);
-        hotel.addConferenceRooms(conferenceRoom);
+        conferenceRoom1 = new ConferenceRoom(1, "North Conference Room", 250.00 );
+        conferenceRoom2 = new ConferenceRoom(1, "South Conference Room", 250.00 );
+        diningRoom1 = new DiningRoom(1, "Fine Dining");
+        diningRoom2 = new DiningRoom(1, "Carvery");
+        hotel.addDiningRooms(diningRoom1);
+        hotel.addDiningRooms(diningRoom2);
+        hotel.addConferenceRooms(conferenceRoom1);
+        hotel.addConferenceRooms(conferenceRoom2);
         hotel.addBedRooms(bedRoom1);
         hotel.addBedRooms(bedRoom2);
         guest1 = new Guest();
@@ -42,64 +48,76 @@ public class HotelTest {
 
     @Test
     public void canAddConferenceRoom() {
-        assertEquals(1, hotel.countConferenceRooms());
+        assertEquals(2, hotel.countConferenceRooms());
     }
 
     @Test
     public void canAddDiningRoom() {
-        assertEquals(1, hotel.countDiningRooms());
+        assertEquals(2, hotel.countDiningRooms());
     }
 
     @Test
     public void canAddGuestToBedRoom() {
-        hotel.checkGuestIn(bedRoom1, guest1, 1);
+        hotel.checkGuestInToBedRoom(bedRoom1, guest1, 1);
         assertEquals(1, bedRoom1.countGuestsInRoom());
     }
 
     @Test
     public void willNotAddToBedRoomWhenFull() {
-        hotel.checkGuestIn(bedRoom1, guest1, 1);
-        hotel.checkGuestIn(bedRoom1, guest2, 1);
+        hotel.checkGuestInToBedRoom(bedRoom1, guest1, 1);
+        hotel.checkGuestInToBedRoom(bedRoom1, guest2, 1);
         assertEquals(1, bedRoom1.countGuestsInRoom());
     }
 
     @Test
     public void canAddGuestToConferenceRoom() {
-        hotel.admitGuestToRoom(conferenceRoom, guest1);
-        assertEquals(1, conferenceRoom.countGuestsInRoom());
+        hotel.admitGuestToRoom(conferenceRoom1, guest1);
+        assertEquals(1, conferenceRoom1.countGuestsInRoom());
     }
 
     @Test
     public void willNotAddToConferenceRoomWhenFull() {
-        hotel.admitGuestToRoom(conferenceRoom, guest1);
-        hotel.admitGuestToRoom(conferenceRoom, guest2);
-        assertEquals(1, conferenceRoom.countGuestsInRoom());
+        hotel.admitGuestToRoom(conferenceRoom1, guest1);
+        hotel.admitGuestToRoom(conferenceRoom1, guest2);
+        assertEquals(1, conferenceRoom1.countGuestsInRoom());
     }
 
     @Test
     public void canAddGuestToDiningRoom() {
-        hotel.admitGuestToRoom(diningRoom, guest1);
-        assertEquals(1, diningRoom.countGuestsInRoom());
+        hotel.admitGuestToRoom(diningRoom1, guest1);
+        assertEquals(1, diningRoom1.countGuestsInRoom());
     }
 
     @Test
     public void willNotAddToDiningRoomWhenFull() {
-        hotel.admitGuestToRoom(diningRoom, guest1);
-        hotel.admitGuestToRoom(diningRoom, guest2);
-        assertEquals(1, diningRoom.countGuestsInRoom());
+        hotel.admitGuestToRoom(diningRoom1, guest1);
+        hotel.admitGuestToRoom(diningRoom1, guest2);
+        assertEquals(1, diningRoom1.countGuestsInRoom());
     }
 
     @Test
     public void canRemoveGuestFromRoom() {
-        hotel.checkGuestIn(bedRoom1, guest1, 1);
+        hotel.checkGuestInToBedRoom(bedRoom1, guest1, 1);
         hotel.checkGuestOut(bedRoom1);
         assertEquals(0, bedRoom1.countGuestsInRoom());
     }
 
     @Test
     public void canGetVacantBedRooms() {
-        hotel.checkGuestIn(bedRoom1, guest1, 1);
+        hotel.checkGuestInToBedRoom(bedRoom1, guest1, 1);
         assertEquals(1, hotel.getVacantBedRooms().size());
+    }
+
+    @Test
+    public void canGetVacantConferenceRooms() {
+        hotel.admitGuestToRoom(conferenceRoom1, guest1);
+        assertEquals(1, hotel.getVacantConferenceRooms().size());
+    }
+
+    @Test
+    public void canGetVacantDiningRooms() {
+        hotel.admitGuestToRoom(diningRoom1, guest1);
+        assertEquals(1, hotel.getVacantDiningRooms().size());
     }
 
 }
